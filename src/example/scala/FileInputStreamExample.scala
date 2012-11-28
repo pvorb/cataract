d145ca9597186178b707484d360e69658da57726
Paul Vorbach <paul@vorb.de>
2012-11-28 12:23:22 +0100

    Refactor examples

---
+++ src/example/scala/FileInputStreamExample.scala
@@ -0,0 +1,36 @@
+/*                  __                              __ 
+**   _____ ____ _ _/ /_ ____ _ _ ___ ____ _ _____ _/ /_
+**  / ___// __ `//  __// __ `// ___// __ `//  __//  __/
+** / /__ / /_/ / / /_ / /_/ // /   / /_/ // /__  / /_
+** \___/ \__,_/  \__/ \__,_//_/    \__,_/ \___/  \__/
+**
+**                                 Cataract Evented I/O
+**                               (c) 2012, Paul Vorbach
+*/
+
+import cataract.event.Data
+import cataract.fs.FileInputStream
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import java.nio.file.Paths
+
+/**
+ *
+ *
+ * @author Paul Vorbach
+ */
+object FileInputStreamExample extends App {
+
+  val utf8Decoder = Charset.forName("UTF-8").newDecoder()
+
+  FileInputStream.create(Paths.get("src/test/resources", "test.txt")) {
+    case Data(buf: ByteBuffer) => {
+      utf8Decoder.reset()
+      print(utf8Decoder.decode(buf))
+    }
+    case _ => System.exit(0)
+  }
+
+  Thread.currentThread().join()
+}
--- src/example/scala/SampleServer.scala
+++ src/example/scala/SampleServer.scala
@@ -1,9 +1,16 @@
-package cataract.example.http
+/*                  __                              __ 
+**   _____ ____ _ _/ /_ ____ _ _ ___ ____ _ _____ _/ /_
+**  / ___// __ `//  __// __ `// ___// __ `//  __//  __/
+** / /__ / /_/ / / /_ / /_/ // /   / /_/ // /__  / /_
+** \___/ \__,_/  \__/ \__,_//_/    \__,_/ \___/  \__/
+**
+**                                 Cataract Evented I/O
+**                               (c) 2012, Paul Vorbach
+*/
 
 import cataract.event.Open
 import cataract.web.http.Server
 import cataract.web.http.Server.Request
-
 import java.net.InetSocketAddress
 
 object SampleServer extends App {
